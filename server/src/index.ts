8adfb05cf99ddb4ee86f6b1b92c1f825da6f759c
wenzel audrin barboza <wenzelbarboza@gmail.com>
2024-11-08 01:02:39 +0200

    deploy error fix back-end 0.1

--- server/src/index.ts
+++ server/src/index.ts
@@ -9,7 +9,14 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+);
 app.use(helmet());
 app.use(morgan("common"));
 // app.use(express.static("public"));
